c34995d8703543a1443e8d2d8f92ba88d4f8568f
Geyslan Gregório <geyslan@gmail.com>
2025-05-24 18:20:25 -0300

    initial commit

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # commands to run in interactive sessions can go here
+
+    set -U fish_features shared_history
+    function __merge_history --on-event fish_prompt
+        history --merge
+    end
+end
